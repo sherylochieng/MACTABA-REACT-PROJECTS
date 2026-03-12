498a31d1c32027432ee5d90c395ab21fac664f6a
sherylOchieng <sadhiambo998@gmail.com>
2026-03-12 10:11:07 +0300

    add project on filtring ,forms,search Thursday 12th March 2026

--- projects/my-projects/src/App.jsx
+++ projects/my-projects/src/App.jsx
@@ -7,7 +7,7 @@ import ProductCard from './components/week8/thursday/watch-store/landingpage/Wat
 import Footer from './components/week8/thursday/watch-store/landingpage/Footer/footer'
 import Timer from './components/week8/Friday/timer'
 import Registration from './components/week9/Monday-forms/Registration'
-
+import ProductGrid from './components/week9/wednesday-filter/ProductLIst-search/Product'
 function App() {
   return(
     <div>
@@ -17,6 +17,12 @@ function App() {
 
           <h1>REGISTRATION FORM PROJECT,MARCH 9TH 2026</h1>
           <Registration/>
+
+
+          <h1>SEARCH AND FILTER PROJECT PRODUCT GRID WEDNESDAY 11TH MARCH 2026</h1> 
+          <ProductGrid/>
+
+
 
       
     </div>
